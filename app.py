a4682defa1808992b848822481e0ab09b58f0310
hrnp91 <34313130+hrnp91@users.noreply.github.com>
2017-12-24 12:07:34 +0530

    Update app.py
    5000 add

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '5000'))
     except ValueError:
-        PORT = 5555
+        PORT = 5000
     app.run(HOST, PORT)
